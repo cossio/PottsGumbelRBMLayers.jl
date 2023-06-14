24d526d9470a3c4501a77a938c1e68628979e4b7
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2023-06-14 17:20:51 +0200

    potts_to_gumbel, gumbel_to_potts

--- src/util.jl
+++ src/util.jl
@@ -1,11 +1,27 @@
-function _rbm_gumbel(rbm::RBM)
-    vis = rbm.visible isa Potts ? PottsGumbel(rbm.visible) : rbm.visible
-    hid = rbm.hidden isa Potts ? PottsGumbel(rbm.hidden) : rbm.hidden
-    return RBM(vis, hid, rbm.w)
+"""
+    potts_to_gumbel(rbm)
+
+Converts Potts layers to PottsGumbel layers.
+"""
+function potts_to_gumbel(rbm::RBM)
+    visible = potts_to_gumbel(rbm.visible)
+    hidden = potts_to_gumbel(rbm.hidden)
+    return RBM(visible, hidden, rbm.w)
 end
 
-function _rbm_potts(rbm::RBM)
-    vis = rbm.visible isa PottsGumbel ? Potts(rbm.visible) : rbm.visible
-    hid = rbm.hidden isa PottsGumbel ? Potts(rbm.hidden) : rbm.hidden
-    return RBM(vis, hid, rbm.w)
+potts_to_gumbel(layer::Potts) = PottsGumbel(layer)
+potts_to_gumbel(layer::PottsGumbel) = layer
+
+"""
+    gumbel_to_potts(rbm)
+
+Converts PottsGumbel layers to Potts layers.
+"""
+function gumbel_to_potts(rbm::RBM)
+    visible = gumbel_to_potts(rbm.visible)
+    hidden = gumbel_to_potts(rbm.hidden)
+    return RBM(visible, hidden, rbm.w)
 end
+
+gumbel_to_potts(layer::Potts) = layer
+gumbel_to_potts(layer::PottsGumbel) = Potts(layer)
--- src/zerosum.jl
+++ src/zerosum.jl
@@ -1,33 +1,33 @@
-RestrictedBoltzmannMachines.zerosum(rbm::RBM{<:PottsGumbel,<:PottsGumbel}) = _rbm_gumbel(zerosum(_rbm_potts(rbm)))
+RestrictedBoltzmannMachines.zerosum(rbm::RBM{<:PottsGumbel,<:PottsGumbel}) = potts_to_gumbel(zerosum(potts_to_gumbel(rbm)))
 
 function RestrictedBoltzmannMachines.zerosum(rbm::RBM{<:PottsGumbel,<:AbstractLayer})
-    _rbm = zerosum(_rbm_potts(rbm))
+    _rbm = zerosum(potts_to_gumbel(rbm))
     return RBM(PottsGumbel(_rbm.visible), _rbm.hidden, _rbm.w)
 end
 
 function RestrictedBoltzmannMachines.zerosum(rbm::RBM{<:AbstractLayer,<:PottsGumbel})
-    _rbm = zerosum(_rbm_potts(rbm))
+    _rbm = zerosum(potts_to_gumbel(rbm))
     return RBM(_rbm.visible, PottsGumbel(_rbm.hidden), _rbm.w)
 end
 
 function RestrictedBoltzmannMachines.zerosum!(rbm::RBM{<:PottsGumbel, <:PottsGumbel})
-    _rbm_gumbel(zerosum!(_rbm_potts(rbm)))
+    potts_to_gumbel(zerosum!(potts_to_gumbel(rbm)))
 end
 
 function RestrictedBoltzmannMachines.zerosum!(rbm::RBM{<:PottsGumbel, <:AbstractLayer})
-    _rbm = zerosum!(_rbm_potts(rbm))
+    _rbm = zerosum!(potts_to_gumbel(rbm))
     return RBM(PottsGumbel(_rbm.visible), _rbm.hidden, _rbm.w)
 end
 
 function RestrictedBoltzmannMachines.zerosum!(rbm::RBM{<:AbstractLayer, <:PottsGumbel})
-    _rbm = zerosum!(_rbm_potts(rbm))
+    _rbm = zerosum!(potts_to_gumbel(rbm))
     return RBM(_rbm.visible, PottsGumbel(_rbm.hidden), _rbm.w)
 end
 
-RestrictedBoltzmannMachines.zerosum!(∂::∂RBM, rbm::RBM{<:PottsGumbel,<:PottsGumbel}) = zerosum!(∂, _rbm_potts(rbm))
-RestrictedBoltzmannMachines.zerosum!(∂::∂RBM, rbm::RBM{<:AbstractLayer,<:PottsGumbel}) = zerosum!(∂, _rbm_potts(rbm))
-RestrictedBoltzmannMachines.zerosum!(∂::∂RBM, rbm::RBM{<:PottsGumbel,<:AbstractLayer}) = zerosum!(∂, _rbm_potts(rbm))
+RestrictedBoltzmannMachines.zerosum!(∂::∂RBM, rbm::RBM{<:PottsGumbel,<:PottsGumbel}) = zerosum!(∂, potts_to_gumbel(rbm))
+RestrictedBoltzmannMachines.zerosum!(∂::∂RBM, rbm::RBM{<:AbstractLayer,<:PottsGumbel}) = zerosum!(∂, potts_to_gumbel(rbm))
+RestrictedBoltzmannMachines.zerosum!(∂::∂RBM, rbm::RBM{<:PottsGumbel,<:AbstractLayer}) = zerosum!(∂, potts_to_gumbel(rbm))
 
-RestrictedBoltzmannMachines.zerosum_weights(weights::AbstractArray, rbm::RBM{<:PottsGumbel,<:PottsGumbel}) = zerosum_weights(weights, _rbm_potts(rbm))
-RestrictedBoltzmannMachines.zerosum_weights(weights::AbstractArray, rbm::RBM{<:AbstractLayer,<:PottsGumbel}) = zerosum_weights(weights, _rbm_potts(rbm))
-RestrictedBoltzmannMachines.zerosum_weights(weights::AbstractArray, rbm::RBM{<:PottsGumbel,<:AbstractLayer}) = zerosum_weights(weights, _rbm_potts(rbm))
+RestrictedBoltzmannMachines.zerosum_weights(weights::AbstractArray, rbm::RBM{<:PottsGumbel,<:PottsGumbel}) = zerosum_weights(weights, potts_to_gumbel(rbm))
+RestrictedBoltzmannMachines.zerosum_weights(weights::AbstractArray, rbm::RBM{<:AbstractLayer,<:PottsGumbel}) = zerosum_weights(weights, potts_to_gumbel(rbm))
+RestrictedBoltzmannMachines.zerosum_weights(weights::AbstractArray, rbm::RBM{<:PottsGumbel,<:AbstractLayer}) = zerosum_weights(weights, potts_to_gumbel(rbm))
