758f66ed31e4737522db1ffe698d5f1de1d2cc6f
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2023-06-14 13:44:47 +0200

    io wip

---
+++ src/io.jl
@@ -0,0 +1,2 @@
+# this should be here, instead of over at RestrictedBoltzmannMachinesHDF5
+RestrictedBoltzmannMachinesHDF5.construct_layer(::Val{:PottsGumbel}, par::AbstractArray) = PottsGumbel(par)
