ded27fae47fba9e12f8ed20a1248e41106ebe350
Jorge FdCD <jfdecd@icloud.com>
2024-03-28 16:16:40 +0000

    devcontainer

---
+++ .devcontainer/onCreate.sh
@@ -0,0 +1,15 @@
+# install Julia and make sure it's in the PATH of the current shell
+curl -fsSL https://install.julialang.org | sh -s -- --yes 
+
+# Julia startup file
+mkdir -p ~/.julia/config
+cp .devcontainer/startup.jl ~/.julia/config/startup.jl
+
+# Github CLI autocomplete (https://www.ajfriesen.com/github-cli-auto-completion-with-oh-my-zsh/)
+mkdir -p ~/.oh-my-zsh/completions
+/usr/bin/gh completion -s zsh > ~/.oh-my-zsh/completions/_gh
+echo "autoload -U compinit" >> ~/.zshrc
+echo "compinit -i" >> ~/.zshrc
+
+# Install Julia packages, registries, ...
+/home/vscode/.juliaup/bin/julia .devcontainer/onCreate.jl
